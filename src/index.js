953ac47d84df2110a0b19bb65fcc9a42dab43b7f
Vlad Bezsonov <vd115313@gmail.com>
2024-08-06 18:45:02 +0200

    feat: resolved

--- src/index.js
+++ src/index.js
@@ -35,11 +35,41 @@ const MORSE_TABLE = {
     '---..':  '8',
     '----.':  '9',
     '-----':  '0',
+    '**********': ' ',
 };
 
 function decode(expr) {
     // write your solution here
+   const objValues = Object.entries(MORSE_TABLE)
+    const chunks = []
+    const cutChar = []
+    const result = []
+    for(let i = 0; i < expr.length; i += 10) {
+        chunks.push(expr.slice(i, i + 10))
+    }
+    chunks.forEach(code => {
+        let number 
+        const reverse = []
+        number = code.match(/10|11/g)
+        if(number !== null || undefined) {
+            number.forEach(el => {
+                reverse.push(el === '11' ? '-' : '.')  
+            })   
+        } else {
+            cutChar.push('**********')
+        }
+        cutChar.push(reverse.join(''));
+    })
+
+    cutChar.forEach(el => {
+        objValues.forEach(([key, value]) => el === key ? result.push(value) : null)
+    })
+    return result.join('')
+
+
 }
+
+
 
 module.exports = {
     decode
